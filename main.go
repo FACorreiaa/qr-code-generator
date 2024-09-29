b863b4d134d0ee2cc428a8ebddc2f716660c7d66
FACorreiaa <fernandocorreia316@gmail.com>
2024-09-29 21:47:41 +0100

    done

--- main.go
+++ main.go
@@ -1,22 +1,19 @@
 package main
 
 import (
-	"os"
-	"path/filepath"
-
 	"github.com/skip2/go-qrcode"
 )
 
 func main() {
-	assetsDir := "assets"
-	content := filepath.Join(assetsDir, "contact.png")
+	//assetsDir := "assets"
+	//content := filepath.Join(assetsDir, "contact.png")
+	//
+	//err := os.MkdirAll(assetsDir, os.ModePerm)
+	//if err != nil {
+	//	panic("Failed to create assets directory")
+	//}
 
-	err := os.MkdirAll(assetsDir, os.ModePerm)
-	if err != nil {
-		panic("Failed to create assets directory")
-	}
-
-	err = qrcode.WriteFile(content, qrcode.Medium, 256, "qr.png")
+	err := qrcode.WriteFile("https://pamozi.de/contact/", qrcode.Medium, 125, "qr.png")
 	if err != nil {
 		panic(err)
 	}
